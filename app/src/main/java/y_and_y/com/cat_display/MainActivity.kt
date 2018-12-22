eaccf7f88e548b7f167a166ef068688d3a105caf
kkt-yu <ku.k669500@gmail.com>
2018-12-22 16:11:18 +0900

    :tada: initial commit

---
+++ app/src/main/java/y_and_y/com/cat_display/MainActivity.kt
@@ -0,0 +1,12 @@
+package y_and_y.com.cat_display
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
